78171b90defc83bc3e6f44ff4009aa059ca642de
Diego Coronado <dcoronado@able.co>
2024-12-05 22:55:15 +0900

    REPL #ff0

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,17 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	var command, err = bufio.NewReader(os.Stdin).ReadString('\n')
+		// Wait for user input
+		var command, err = bufio.NewReader(os.Stdin).ReadString('\n')
 
-	if err != nil {
-		fmt.Println("error ", err)
-		os.Exit(1)
+		if err != nil {
+			fmt.Println("error ", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
